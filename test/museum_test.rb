9f768dc4d7656932cff97fcbb93dff445e398746
Gvieve <gnuebel84@yahoo.com>
2021-01-07 09:55:53 -0700

    Add recommend_exhibit method to Museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -10,7 +10,10 @@ class MuseumTest < Minitest::Test
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
     @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
     @dmns = Museum.new("Denver Museum of Nature and Science")
   end
 
@@ -26,5 +29,16 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits_to_patrons
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    expected_1 = [@gems_and_minerals, @dead_sea_scrolls]
+    assert_equal expected_1, @dmns.recommend_exhibits(@patron_1)
+    expected_2 = [@imax]
+    assert_equal expected_2, @dmns.recommend_exhibits(@patron_2)
   end
 end
